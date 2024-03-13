13438294886c6ed7ce98441ec959a327a46bb906
Pascalis Sandi <pascalis.sandi09@gmail.com>
2024-03-13 08:53:49 +0700

    praktikum 2-5 J4

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -31,13 +31,48 @@ class UserController extends Controller
             // ];
             // UserModel::create($data);
             
-        $user = UserModel::firstOrNew([
-            'username' => 'manager33',
-            'nama' => 'Manager tiga tiga',
+        // $user = UserModel::firstOrNew([
+        //     'username' => 'manager33',
+        //     'nama' => 'Manager tiga tiga',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+        // // $user->save();
+        // return view('user', ['data' => $user]);
+
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2,
+
         ]);
+
+        $user->username = 'manager12';
+
+        $user->save();
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+        $user->wasChanged(['nama', 'username']);
+        dd($user->wasChanged(['nama', 'username']));
+
+        // $user->isDirty();
+        // $user->isDirty('usernam');
+        // $user->isDirty('nama');
+        // $user->isDirty(['nama', 'username']);
+
+        // $user->isClean();
+        // $user->isClean('username');
+        // $user->isClean('nama');
+        // $user->isClean(['nama', 'username']);
+
         // $user->save();
-        return view('user', ['data' => $user]);
+
+        // $user->isDirty();
+        // $user->isClean();
+        // dd($user->isClean());
     }
 }
