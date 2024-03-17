66fe764d080e94c730654bf2c9a9804e83ef0d4c
Dcrezztopia <pascalis.sandi09@gmail.com>
2024-03-17 21:28:01 +0700

    Menyelesaikan seluruh praktikum dan tugas Jobsheet 5

--- laravel/app/Http/Controllers/KategoriController.php
+++ laravel/app/Http/Controllers/KategoriController.php
@@ -3,23 +3,52 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use App\Models\KategoriModel;
+use App\DataTables\KategoriDataTable;
+use Laravel\Ui\Presets\React;
+
+// use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function index(){
-        // $data = [
-        //     'kategori_kode' => 'SNK',
-        //     'kategori_nama' => 'Snack/Makanan Ringan',
-        //     'created_at' => now()
-        // ];
-        // DB::table('m_kategori')->insert($data);
-        // return 'Insert data baru berhasil';
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
-        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row.' baris';
-        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
-        // return 'Delete data berhasil. Jumlah data yang didelete: ' . $row.' baris';
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $data]);
+    public function index(KategoriDataTable $dataTable){
+        return $dataTable->render('kategori.index');    
+    }
+
+    public function create(){
+        return view('kategori.create');
+    }
+
+    public function store(Request $request){
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori');
+    }
+    public function edit($id)
+    {
+        $kategori = KategoriModel::find($id);
+        return view('kategori.edit', ['data' => $kategori]);
+    }
+
+    public function update($id, Request $request)
+    {
+        $kategori = KategoriModel::find($id);
+
+        $kategori->kategori_kode = $request->kodeKategori;
+        $kategori->kategori_nama = $request->namaKategori;
+
+        $kategori->save();
+
+        return redirect('/kategori');
+    }
+
+    public function delete($id)
+    {
+        $kategori = KategoriModel::find($id);
+        $kategori->delete();
+
+        return redirect('/kategori');
     }
 }
---
+++ laravel/app/Models/BarangModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarangModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'm_barang';
+    protected $primaryKey = 'barang_id';
+    
+    // protected $fillable = ['kategori_kode', 'kategori_nama'];
+
+}
