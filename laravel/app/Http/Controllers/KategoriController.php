934e182eafb7f30696b5d03449a684b50a56e384
Dcrezztopia <pascalis.sandi09@gmail.com>
2024-03-28 14:50:49 +0700

    Menyelesaikan Js6 B

--- laravel/app/Http/Controllers/KategoriController.php
+++ laravel/app/Http/Controllers/KategoriController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\view\view;
 use App\Models\KategoriModel;
 use App\DataTables\KategoriDataTable;
 use Laravel\Ui\Presets\React;
@@ -15,40 +17,44 @@ class KategoriController extends Controller
         return $dataTable->render('kategori.index');    
     }
 
-    public function create(){
+    public function create(): View{
         return view('kategori.create');
     }
 
-    public function store(Request $request){
+    public function store(Request $request): RedirectResponse{
+        $validated = $request->validate([
+            'kategori_kode' => 'required',
+            'kategori_nama' => 'required',
+        ]);
         KategoriModel::create([
-            'kategori_kode' => $request->kodeKategori,
-            'kategori_nama' => $request->namaKategori,
+            'kategori_kode' => $validated['kategori_kode'],
+            'kategori_nama' => $validated['kategori_nama'],
         ]);
         return redirect('/kategori');
     }
-    public function edit($id)
-    {
-        $kategori = KategoriModel::find($id);
-        return view('kategori.edit', ['data' => $kategori]);
-    }
+    // public function edit($id)
+    // {
+    //     $kategori = KategoriModel::find($id);
+    //     return view('kategori.edit', ['data' => $kategori]);
+    // }
 
-    public function update($id, Request $request)
-    {
-        $kategori = KategoriModel::find($id);
+    // public function update($id, Request $request)
+    // {
+    //     $kategori = KategoriModel::find($id);
 
-        $kategori->kategori_kode = $request->kodeKategori;
-        $kategori->kategori_nama = $request->namaKategori;
+    //     $kategori->kategori_kode = $request->kodeKategori;
+    //     $kategori->kategori_nama = $request->namaKategori;
 
-        $kategori->save();
+    //     $kategori->save();
 
-        return redirect('/kategori');
-    }
+    //     return redirect('/kategori');
+    // }
 
-    public function delete($id)
-    {
-        $kategori = KategoriModel::find($id);
-        $kategori->delete();
+    // public function delete($id)
+    // {
+    //     $kategori = KategoriModel::find($id);
+    //     $kategori->delete();
 
-        return redirect('/kategori');
-    }
+    //     return redirect('/kategori');
+    // }
 }
--- laravel/resources/views/kategori/create.blade.php
+++ laravel/resources/views/kategori/create.blade.php
@@ -5,6 +5,15 @@
 @section('content_header_subtitle', 'Create')
 
 @section('content')
+@if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 <div class="container">
     <div class="card card-primary">
         <div class="card-header">
@@ -12,10 +21,17 @@
         </div>
 
         <form method="post" action="../kategori">
+            @csrf
             <div class="card-body">
                 <div class="form-group">
                     <label for="kodeKategori">Kode Kategori</label>
-                    <input type="text" class="form-control" name="kodeKategori" id="kodeKategori" placeholder="Kode kategori, contoh:FOD">
+                    <input type="text" class="form-control
+                    @error('kategori_kode')
+                        is-invalid
+                        @enderror" name="kodeKategori" id="kodeKategori" placeholder="Kode kategori, contoh:FOD">
+                        @error('kategori_kode')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                 </div>
                 <div class="form-group">
                     <label for="namaKategori">Nama Kategori</label>
