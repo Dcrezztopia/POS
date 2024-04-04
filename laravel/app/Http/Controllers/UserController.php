6174ac4961df1059de54cc29c2174705cb800412
Dcrezztopia <pascalis.sandi09@gmail.com>
2024-04-04 17:05:48 +0700

    Menyelesaikan JS7 Praktikum 1 & 2

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -5,78 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\UserModel;
+use App\Models\LevelModel;
+use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
     public function index(){
-        // $data = [
-        // 'username' => 'customer-1',
-        // 'nama' => 'pelanggan',
-        // 'password' => Hash::make('12345'),
-        // 'level_id' => 4,];
-        // UserModel::insert($data);
-        // $data = [
-            //     'nama' => 'Pelanggan Pertama',
-            // ];
-            // UserModel::where('username', 'customer-1') -> update($data);
-            
-            // $user = UserModel::all();
-            // return view('user', ['data' => $user]);
-            
-            // $data = [
-            //     'level_id' => 2,
-            //     'username' => 'manager_tiga',
-            //     'nama' => 'Manager 3',
-            //     'password' => Hash::make('12345')
-            // ];
-            // UserModel::create($data);
-            
-        // $user = UserModel::firstOrNew([
-        //     'username' => 'manager33',
-        //     'nama' => 'Manager tiga tiga',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 2,
-        // ]);
-        // // $user->save();
-        // return view('user', ['data' => $user]);
+       $breadcrumb = (object) [
+        'title' => 'Daftar User',
+        'list' => ['Home', 'User']
+       ];
 
-        // $user = UserModel::create([
-        //     'username' => 'manager11',
-        //     'nama' => 'Manager11',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 2,
+       $page = (object) [
+        'title' => 'Daftar user yang terdaftar dalam sistem'
+       ];
 
-        // ]);
+       $activeMenu = 'user';
 
-        // $user->username = 'manager12';
-
-        // $user->save();
-
-        // $user->wasChanged();
-        // $user->wasChanged('username');
-        // $user->wasChanged(['username', 'level_id']);
-        // $user->wasChanged('nama');
-        // $user->wasChanged(['nama', 'username']);
-        // dd($user->wasChanged(['nama', 'username']));
-
-        // $user->isDirty();
-        // $user->isDirty('usernam');
-        // $user->isDirty('nama');
-        // $user->isDirty(['nama', 'username']);
-
-        // $user->isClean();
-        // $user->isClean('username');
-        // $user->isClean('nama');
-        // $user->isClean(['nama', 'username']);
-
-        // $user->save();
-
-        // $user->isDirty();
-        // $user->isClean();
-        // dd($user->isClean());
-
-        $user = UserModel::all();
-        return view('user', ['data'=>$user]);
+       return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
     }
     public function tambah(){
         return view('user_tambah');
