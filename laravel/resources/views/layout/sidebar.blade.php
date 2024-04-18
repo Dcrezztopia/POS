75b3836b500c8f85d309bae83407a96f1cdf4d94
Dcrezztopia <pascalis.sandi09@gmail.com>
2024-04-18 10:16:23 +0700

    Lupa commit, menyelesaikan tugas JS7

--- laravel/resources/views/layout/sidebar.blade.php
+++ laravel/resources/views/layout/sidebar.blade.php
@@ -91,69 +91,14 @@
           </a>
         </li>
         <li class="nav-item">
-          <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-edit"></i>
+          <a href="{{url('/penjualan')}}" class="nav-link {{ ($activeMenu == 'penjualan')? 'active' : ''}}">
+            <i class="nav-icon fas fa-cash-register"></i>
             <p>
-              Forms
-              <i class="fas fa-angle-left right"></i>
+              Transaksi Penjualan
             </p>
           </a>
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a href="../forms/general.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>General Elements</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="../forms/advanced.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Advanced Elements</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="../forms/editors.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Editors</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="../forms/validation.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Validation</p>
-              </a>
-            </li>
-          </ul>
         </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">
-            <i class="nav-icon fas fa-table"></i>
-            <p>
-              Tables
-              <i class="fas fa-angle-left right"></i>
-            </p>
-          </a>
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a href="../tables/simple.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Simple Tables</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="../tables/data.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>DataTables</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="../tables/jsgrid.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>jsGrid</p>
-              </a>
-            </li>
-          </ul>
-        </li>
+        
         <li class="nav-header">EXAMPLES</li>
         <li class="nav-item">
           <a href="../calendar.html" class="nav-link">
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\FBController;
-use App\Http\Controllers\BHController;
-use App\Http\Controllers\HCController;
-use App\Http\Controllers\BKController;
+
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\StokController;
+use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\POSController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
@@ -39,6 +39,61 @@ Route::group(['prefix' => 'user'], function (){
     Route::delete('/{id}', [UserController::class, 'destroy']);
 });
 
+Route::group(['prefix' => 'level'], function (){
+    Route::get('/', [LevelController::class, 'index']);
+    Route::post('/list', [LevelController::class, 'list']);
+    Route::get('/create', [LevelController::class, 'create']);
+    Route::post('/', [LevelController::class, 'store']); 
+    Route::get('/{id}', [LevelController::class, 'show']); 
+    Route::get('/{id}/edit', [LevelController::class, 'edit']);
+    Route::put('/{id}', [LevelController::class, 'update']);
+    Route::delete('/{id}', [LevelController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'kategori'], function () {
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::post('/list', [KategoriController::class, 'list']);
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{id}', [KategoriController::class, 'show']);
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+    Route::put('/{id}', [KategoriController::class, 'update']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'barang'], function () {
+    Route::get('/', [BarangController::class, 'index']);
+    Route::post('/list', [BarangController::class, 'list']);
+    Route::get('/create', [BarangController::class, 'create']);
+    Route::post('/', [BarangController::class, 'store']);
+    Route::get('/{id}', [BarangController::class, 'show']);
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);
+    Route::put('/{id}', [BarangController::class, 'update']);
+    Route::delete('/{id}', [BarangController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'stok'], function () {
+    Route::get('/', [StokController::class, 'index']);
+    Route::post('/list', [StokController::class, 'list']);
+    Route::get('/create', [StokController::class, 'create']);
+    Route::post('/', [StokController::class, 'store']);
+    Route::get('/{id}', [StokController::class, 'show']);
+    Route::get('/{id}/edit', [StokController::class, 'edit']);
+    Route::put('/{id}', [StokController::class, 'update']);
+    Route::delete('/{id}', [StokController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'penjualan'], function () {
+    Route::get('/', [PenjualanController::class, 'index']);
+    Route::post('/list', [PenjualanController::class, 'list']);
+    Route::get('/create', [PenjualanController::class, 'create']);
+    Route::get('/get-harga/{id}', [PenjualanController::class, 'getHarga']);
+    Route::post('/', [PenjualanController::class, 'store']);
+    Route::get('/{id}', [PenjualanController::class, 'show']);
+    Route::get('/{id}/edit', [PenjualanController::class, 'edit']);
+    Route::put('/{id}', [PenjualanController::class, 'update']);
+    Route::delete('/{id}', [PenjualanController::class, 'destroy']);
+});
 // Route::get('/user/{id}/name/{name}', function ($id, $name) {
 //     return '<h1>INFORMASI USER</h1>
 //             Nama: '.$name.
@@ -47,13 +102,13 @@ Route::group(['prefix' => 'user'], function (){
 
 Route::get('/sales', [TransaksiController::class, 'landing']);
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index'])->name('manage.kategori');
-Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
-Route::post('/kategori', [KategoriController::class, 'store']);
-Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit']);
-Route::put('/kategori/{id}', [KategoriController::class, 'update']);
-Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete']);
+// Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index'])->name('manage.kategori');
+// Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+// Route::post('/kategori', [KategoriController::class, 'store']);
+// Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit']);
+// Route::put('/kategori/{id}', [KategoriController::class, 'update']);
+// Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete']);
 
 
 // Route::resource('m_user', POSController::class);
