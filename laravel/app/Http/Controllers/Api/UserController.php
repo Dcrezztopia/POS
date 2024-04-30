7c14cb16ce591069cb53c720342ed139e5b2f20c
Pascalis Sandi <pascalis.sandi09@gmail.com>
2024-04-30 19:12:16 +0700

    Menyelesaikan Tugas JS10

---
+++ laravel/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\UserModel;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return UserModel::all();
+    }
+
+    public function store(Request $request)
+    {
+        $user = UserModel::create($request->all());
+        return response()->json($user, 201);
+    }
+
+    public function show(UserModel $user)
+    {
+        return response()->json([
+            'status_code' => 200,
+            'data' => $user
+        ]);
+    }
+
+    public function update(Request $request, UserModel $user)
+    {
+        $user->update($request->all());
+        return UserModel::find($user);
+    }
+
+    public function destroy(UserModel $user)
+    {
+        $user->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Data terhapus',
+        ]);
+    }
+}
