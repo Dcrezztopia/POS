a699ee324542a4e286713fbed01a9fcbf90a5559
Pascalis Sandi <pascalis.sandi09@gmail.com>
2024-04-30 16:49:40 +0700

    Menyelesaikan Prak 3 JS10

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/logout', LogoutController::class)->name('logout');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
